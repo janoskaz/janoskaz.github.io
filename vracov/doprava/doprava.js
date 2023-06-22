46e13a1e1cda174a107a63aa9de368021f242c11
janoskaz <zbynek.janoska@gmail.com>
2023-06-22 17:55:27 +0200

    [ZJ] mapa doprava Vracov

---
+++ vracov/doprava/doprava.js
@@ -0,0 +1,246 @@
+var jsonObj = {
+    "center": {
+        "lat": 48.9775,
+        "lon": 17.215
+    },
+    "zoom": {
+        "default": 16,
+        "min": 14,
+        "max": 17
+    },
+    "bounds": {
+        "topLeft": 48.99,
+        "bottomLeft": 17.16,
+        "topRight": 48.95,
+        "bottomRight": 17.24
+    },
+    "markers": [{
+            "lat": 48.9713272,
+            "lon": 17.2022003,
+            "text": "Chybějící chodník v ulici Olšíčská"
+        },
+        {
+            "lat": 48.9728344,
+            "lon": 17.2001189,
+            "text": "Chybějící chodník v ulici Borová"
+        },
+        {
+            "lat": 48.9746867,
+            "lon": 17.2043031,
+            "text": "Chybějící chodník v ulici Ernestovská"
+        },
+        {
+            "lat": 48.9737006,
+            "lon": 17.2156328,
+            "text": "Stání vozidel na vozovce v nepřehledném úseku ulice Okružní"
+        },
+        {
+            "lat": 48.9715667,
+            "lon": 17.2147422,
+            "text": "Nedodržování přednosti zprava v ulici Dlouhá"
+        },
+        {
+            "lat": 48.9729331,
+            "lon": 17.2160406,
+            "text": "Nedodržování přednosti zprava na křižovatce Dlouhá / Strážnická"
+        },
+        {
+            "lat": 48.9702003,
+            "lon": 17.2141414,
+            "text": "Nedodržování přednosti zprava na křižovatce Dlouhá / Starce"
+        },
+        {
+            "lat": 48.9726583,
+            "lon": 17.2154181,
+            "text": "Špatný rozhled v úzké části ulice Dlouhá"
+        },
+        {
+            "lat": 48.9747078,
+            "lon": 17.2070711,
+            "text": "Chybějící přechod pro chodce u bývalého Koloniálu"
+        },
+        {
+            "lat": 48.9795808,
+            "lon": 17.2253961,
+            "text": "Nepřehledná křižovatka Jana Husa / Cihelná, nedodržování přednosti zprava"
+        },
+        {
+            "lat": 48.9796158,
+            "lon": 17.2263617,
+            "text": "Nepřehledná křižovatka Jana Husa / Polní, nedodržování přednosti zprava"
+        },
+        {
+            "lat": 48.9723344,
+            "lon": 17.2210617,
+            "text": "Nepřehledná křižovatka Kučovanská / Boženy Němcové / A. Petržely / Tyršova, nedodržování přednosti zprava"
+        },
+        {
+            "lat": 48.9727569,
+            "lon": 17.2196453,
+            "text": "Nepřehledná křižovatka Osmecká / Sportovní / A. Petržely / P. Bezruče, nedodržování přednosti zprava"
+        },
+        {
+            "lat": 48.9732006,
+            "lon": 17.2002261,
+            "text": "Nedodržování přednosti zprava na křižovatce Borová / Lomená"
+        },
+        {
+            "lat": 48.9797708,
+            "lon": 17.2289150,
+            "text": "Nedodržování předepsané rychlosti v ulici Jana Husa"
+        },
+        {
+            "lat": 48.9734472,
+            "lon": 17.2165769,
+            "text": "Přechod pro chodce na nepřehledném místě"
+        },
+        {
+            "lat": 48.9734472,
+            "lon": 17.2165769,
+            "text": "Přechod pro chodce na nepřehledném místě - ulice Okružní, u křižovatky se Strážnickou"
+        },
+        {
+            "lat": 48.9740350,
+            "lon": 17.2148603,
+            "text": "Přechod pro chodce na nepřehledném místě - ulice Okružní, u křižovatky s Vypálenou"
+        },
+        {
+            "lat": 48.9740350,
+            "lon": 17.2148603,
+            "text": "Přechod pro chodce na nepřehledném místě - ulice Okružní, u křižovatky s Vypálenou"
+        },
+        {
+            "lat": 48.9766125,
+            "lon": 17.2085411,
+            "text": "Přechod pro chodce na nepřehledném místě, je hojně využívaný dětmi - ulice Drahy, u křižovatky s Okružní"
+        },
+        {
+            "lat": 48.9760458,
+            "lon": 17.2097050,
+            "text": "Stojící auta v blízkosti přechodu brání rozhledu"
+        },
+        {
+            "lat": 48.9794856,
+            "lon": 17.2243553,
+            "text": "Přechod pro chodce na mostku, v zimě dochází k namrzání vozovky"
+        },
+        {
+            "lat": 48.9794856,
+            "lon": 17.2243553,
+            "text": "Přechod pro chodce na mostku, v zimě dochází k namrzání vozovky"
+        },
+        {
+            "lat": 48.9706475,
+            "lon": 17.2087664,
+            "text": "Ulice Dlouhá je úzká, problém s vyhýbáním protijedoucích vozidel"
+        },
+        {
+            "lat": 48.9763450,
+            "lon": 17.2108261,
+            "text": "Ulice Dolní řádky je úzká, problém s vyhýbáním protijedoucích vozidel"
+        },
+        {
+            "lat": 48.976056,
+            "lon": 17.2108156,
+            "text": "Ulice Horní řádky je úzká, problém s vyhýbáním protijedoucích vozidel"
+        },
+        {
+            "lat": 48.9680136,
+            "lon": 17.2080475,
+            "text": "Ulice Lesní je úzká, problém s vyhýbáním protijedoucích vozidel"
+        },
+        {
+            "lat": 48.9714222,
+            "lon": 17.2111053,
+            "text": "Nedodržování přednosti zprava na křižovatce Dlouhá / Úzká"
+        },
+        {
+            "lat": 48.9734717,
+            "lon": 17.2162658,
+            "text": "Špatné rozhledové poměry - křižovatka Okružní / Strážnická"
+        },
+        {
+            "lat": 48.9734717,
+            "lon": 17.2162658,
+            "text": "Špatné rozhledové poměry - křižovatka Okružní / Strážnická"
+        },
+        {
+            "lat": 48.9755775,
+            "lon": 17.2187228,
+            "text": "Rozšíření chodníku u školky o cyklopruh"
+        },
+        {
+            "lat": 48.9718167,
+            "lon": 17.2118561,
+            "text": "Při odbočování z ulice Lipinská do ulice Dlouhá problém vyhnout se protijedoucímu vozidlu"
+        },
+        {
+            "lat": 48.9747744,
+            "lon": 17.2069639,
+            "text": "Při odbočování z ulice Ernestovská do ulice Drahy špatné rozhledové poměry, pokud před Koloniálem stojí zaparkovaná vozidla"
+        },
+        {
+            "lat": 48.9751056,
+            "lon": 17.2110731,
+            "text": "Stojící vozidla v jednosměrné části ulice Kollárova brání rozhledu"
+        },
+        {
+            "lat": 48.9751056,
+            "lon": 17.2110731,
+            "text": "Stojící vozidla v jednosměrné části ulice Kollárova brání rozhledu"
+        },
+        {
+            "lat": 48.9817247,
+            "lon": 17.2078328,
+            "text": "Chybějící přechod pro chodce u autobusové zastávky Slokov"
+        },
+        {
+            "lat": 48.9790703,
+            "lon": 17.2128325,
+            "text": "Při odbočování z ulice Sokolská do ulice Jiráskova špatné rozhledové poměry"
+        },
+        {
+            "lat": 48.9790703,
+            "lon": 17.2128325,
+            "text": "Při odbočování z ulice Sokolská do ulice Jiráskova špatné rozhledové poměry"
+        },
+        {
+            "lat": 48.9683586,
+            "lon": 17.2044103,
+            "text": "Hluboké výmoly na ulici Hodonínská"
+        },
+        {
+            "lat": 48.9739858,
+            "lon": 17.2150106,
+            "text": "Špatné rozhledové poměry při výjezdu z ulice Vypálené do ulice Okružní"
+        },
+        {
+            "lat": 48.9680417,
+            "lon": 17.2176283,
+            "text": "Nerovný povrch na komunikaci (vlny)"
+        },
+        {
+            "lat": 48.9698236,
+            "lon": 17.2049683,
+            "text": "Nepřehledné napojení ulice Hodonínská na ulici Drahy - ostrý úhel napojení"
+        }
+    ]
+ 
+}
+/*init map*/
+var layer = new L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+var map   = new L.map('map', {
+    center: new L.LatLng(jsonObj.center.lat, jsonObj.center.lon),
+    zoom:   jsonObj.zoom.default,
+    minZoom: jsonObj.zoom.min,
+    maxZoom: jsonObj.zoom.max
+});
+map.setMaxBounds(L.latLngBounds(L.latLng(jsonObj.bounds.topLeft, jsonObj.bounds.bottomLeft),L.latLng(jsonObj.bounds.topRight, jsonObj.bounds.bottomRight)));
+map.addLayer(layer);
+
+for (var i = 0; i < jsonObj.markers.length; i++) {
+    var obj = jsonObj.markers[i];
+    var marker = new L.marker([obj.lat, obj.lon]);
+    marker.bindPopup(obj.text);
+    map.addLayer(marker);
+}
